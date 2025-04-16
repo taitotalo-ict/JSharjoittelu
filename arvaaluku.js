da3730de897da697d1696bfdfe02dc3bc7927d3b
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-04-16 12:51:32 +0300

    20250416_125131

--- arvaaluku.js
+++ arvaaluku.js
@@ -1,9 +1,36 @@
-const numero = 67;
+let numero = Math.floor(Math.random()*100);
 const inputElem = document.getElementById('input');
 const messageElem = document.getElementById('viesti');
+const buttonElem = document.getElementById('painike');
+const arvauksetElem = document.getElementById('arvaukset');
 
+document.getElementById('reset').addEventListener('click', () => {
+    numero = Math.floor(Math.random()*100);
+    console.log(`Uusi numero on ${numero}`);
+    inputElem.value = '';
+    messageElem.textContent = '';
+    arvauksetElem.textContent = '';
+});
 
-document.getElementById('painike').addEventListener('click', () => {
+inputElem.addEventListener('keypress', (event) => {
+    console.log(event);
+    if (event.key == 'Enter') {
+        buttonElem.click();
+    }
+});
+
+// inputElem.addEventListener('keydown', (event) => {
+//     console.log(event);
+//     if ((! '0123456789'.includes(event.key)) && event.key != 'Backspace')
+//         event.preventDefault();
+// });
+
+buttonElem.addEventListener('click', () => {
+    if (inputElem.value === '') {
+        messageElem.textContent = 'Syötä numero input kenttään!';
+        return
+    }
+    
     let userNumber = Number(inputElem.value);
 
     if (userNumber > numero) {
@@ -12,5 +39,11 @@ document.getElementById('painike').addEventListener('click', () => {
         messageElem.textContent = 'Liian pieni';
     } else {
         messageElem.textContent = 'Kyllä tuo oli se numero.';
+    }
+
+    if (arvauksetElem.textContent == '') {
+        arvauksetElem.textContent = userNumber;
+    } else {
+        arvauksetElem.textContent += ', ' + userNumber;
     }
 });
